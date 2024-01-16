b291b94e42254595d9122df68651fda53c768275
charllab <developer.charl@gmail.com>
2024-01-16 15:20:47 -0800

    Project start

---
+++ src/themes/northernlightsforesteducation/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,600;0,800;1,300;1,600;1,800&display=swap" rel="stylesheet">
+
+    <script src="https://kit.fontawesome.com/61d303198f.js" crossorigin="anonymous"></script>
+
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+
+    <?php get_template_part('partials/header/hero-nav-overlay'); ?>
